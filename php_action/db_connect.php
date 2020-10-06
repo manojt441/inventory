c33048e862f9b4643cc1e2decac943803f1a7d58
manoj thakur <manojthakur1996t@gmail.com>
2020-10-06 19:38:17 +0530

    changes

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -4,7 +4,7 @@ $localhost = "localhost";
 $username = "root";
 $password = "";
 $dbname = "store";
-$store_url = "http://localhost/php-inventory-management-system/";
+$store_url = "http://localhost/inventory/";
 // db connection
 $connect = new mysqli($localhost, $username, $password, $dbname);
 // check connection
